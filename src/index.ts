a415850dbdd86199a0b56989c793a116f7c73aa1
FriendsFerdinand <friendsferdinand@protonmail.com>
2023-02-13 15:53:09 +0000

    some more comments on claiming funds

--- src/index.ts
+++ src/index.ts
@@ -16,21 +16,21 @@ import { blockchainRequest, opDropASM } from "./bitcoin.js";
   let block = await blockchainRequest("getblock", [blocks[0]]);
   let tx = await blockchainRequest("getrawtransaction", [block.tx[0], true]);
 
+  const data = Buffer.from("0000000000000000000000000000000000000000","hex");
+
   // Generating asm of Bitcoin script
   // @params
   // hash of a Stacks address or contract
   // public key of the recipient (in case of sBTC, the shared wallet)
   // FORMAT: `<data> OP_DROP OP_DUP OP_HASH160 <recipientPublicKey> OP_EQUALVERIFY OP_CHECKSIG`
-  let asm = opDropASM(
-    Buffer.from("0000000000000000000000000000000000000000","hex"),
-    crypto.hash160(recipientBtcSigner.publicKey)
-  );
+  let asm = opDropASM(data, crypto.hash160(recipientBtcSigner.publicKey));
 
   const script = bScript.fromASM(asm);
   const payment = payments.p2sh({ redeem: { output: script, network }, network });
+  console.log(payment.address);
 
   let hash = bAddress.fromBase58Check(senderPayment.address!).hash.toString("hex");
-  let unspentTxout = tx.vout.filter((val: any) => val.scriptPubKey.hex.match(hash));
+  let unspentTxout = tx.vout.filter((val: any) => val.scriptPubKey.hex.match(senderPayment.hash?.toString("hex")));
 
   const fundingAmount = 100_000_000;
   
@@ -54,6 +54,10 @@ import { blockchainRequest, opDropASM } from "./bitcoin.js";
   psbt.finalizeInput(0);
 
   let result = await blockchainRequest('sendrawtransaction', [psbt.extractTransaction(true).toHex()]);
+
+  // AFTER the transaction is confirmed, Stacks node needs to be notified of the data in the address.
+  // Then, the address can be verifies by regenerating the redeemscript:
+  // <data> OP_DROP OP_DUP OP_HASH160 <recipientPublicKey> OP_EQUALVERIFY OP_CHECKSIG`
 
   // START Claiming process by sBTC signers
   tx = await blockchainRequest("getrawtransaction", [result, true]);
@@ -81,6 +85,7 @@ import { blockchainRequest, opDropASM } from "./bitcoin.js";
       partialSigs![0].signature,
       recipientBtcSigner.publicKey
     ]);
+    // <sig> <recipientPublicKey>
     const payment = payments.p2sh({
       redeem: {
         output: script,
@@ -97,5 +102,6 @@ import { blockchainRequest, opDropASM } from "./bitcoin.js";
   await blockchainRequest("generatetoaddress", [5, senderPayment.address]);
   
   tx = await blockchainRequest("getrawtransaction", [resultTxId, true]);
+  console.log(tx);
   console.log("Succesfully sent funds to public key");
 })();
